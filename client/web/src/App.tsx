3eaa562c5b47b81962b56513187d423feba231eb
Николай Лелюх <Nikolay.Lelyukh@vittecon.onmicrosoft.com>
2021-03-31 01:47:21 +0300

    dev - web - add material and node-saas

--- client/web/src/App.tsx
+++ client/web/src/App.tsx
@@ -1,24 +1,23 @@
+import { Button, Grid } from '@material-ui/core';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div> 
+      <Grid container>
+        <Grid item xs={1}>
+          {"Happy coding :)"}
+        </Grid>
+        <Grid item xs={1}>
+          <Button 
+              variant="contained" 
+              color="secondary">
+              {"Let`s go!"}
+          </Button>
+        </Grid>
+      </Grid>
+      </div>
     </div>
   );
 }
