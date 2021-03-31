13c0f5a8dbba2b8432d598d3e1197c6b03eb5362
Николай Лелюх <Nikolay.Lelyukh@vittecon.onmicrosoft.com>
2021-03-31 20:08:30 +0300

    feat - web - add markdown editor

--- client/web/src/App.tsx
+++ client/web/src/App.tsx
@@ -1,5 +1,33 @@
 import { Button, Grid } from '@material-ui/core';
 import React from 'react';
+import Vditor from 'vditor'
+import "vditor/src/assets/scss/index.scss"
+
+const e = React.createElement
+class MarkdownEditor extends React.Component {
+
+  componentDidMount () {
+    const vditor = new Vditor('vditor', {
+      height: 360,
+      toolbarConfig: {
+        pin: true,
+      },
+      cache: {
+        enable: false,
+      },
+      after () {
+        vditor.setValue('Hello, Vditor + React!')
+      },
+    })
+  }
+
+  render () {
+    return e(
+      'div',
+      {id: 'vditor'},
+    )
+  }
+}
 
 function App() {
   return (
@@ -15,6 +43,9 @@ function App() {
               color="secondary">
               {"Let`s go!"}
           </Button>
+        </Grid>
+        <Grid item xs={6}>
+        <MarkdownEditor />
         </Grid>
       </Grid>
       </div>
