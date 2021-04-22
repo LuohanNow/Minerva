7892f5927ead5b3877344ad4cd0067fb5c7f783f
Николай Лелюх <Nikolay.Lelyukh@vittecon.onmicrosoft.com>
2021-04-23 02:50:11 +0300

    feat - web - add search

--- client/web/src/components/ResultPanel/ResultPanel.tsx
+++ client/web/src/components/ResultPanel/ResultPanel.tsx
@@ -1,15 +1,22 @@
 import "./ResultPanel.scss"
-import { InputBase, List, ListItem, ListItemText } from '@material-ui/core';
+import { Button, InputBase, List, ListItem, ListItemText } from '@material-ui/core';
 import { Search } from '@material-ui/icons';
 import { t } from '../../localization';
 import { Document } from "../../models/Document";
+import { KeyboardEvent } from "react";
 
 type ResultPanelProps = {
   documentItems: Array<Document>;
   onResultItemClick(id: string): void;
+  onSearch(searchText: string): void;
 }
 
 export function ResultPanel(props: ResultPanelProps) {
+  function onEnterKeyDown(e: KeyboardEvent<HTMLTextAreaElement | HTMLInputElement>): void {
+    if(e.code === "Enter")
+      props.onSearch(e.currentTarget.value as string);
+  }
+
   return (
     <div className="result-panel">
         <div className="search">
@@ -17,6 +24,7 @@ export function ResultPanel(props: ResultPanelProps) {
             <Search />
           </div>
           <InputBase
+            onKeyDown={onEnterKeyDown}
             placeholder={t("search")}
             className="search-field"
           />
